a72b86a77cb1a1fedbd636ef9f6c113131feb6f1
Alexander Kuleshov <kuleshovmail@gmail.com>
2022-06-27 00:01:59 +0600

    add some basic cli
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,56 @@
+use std::env;
+use std::process;
+use std::path::{Path};
+
+use getopts::{HasArg, Options, Occur};
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn print_usage(opts: Options) {
+    let brief = format!("Usage: whirl -s SCRIPT [options]");
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let argv: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+
+    opts.opt("s", "script", "path to the script with a scenario", "SCRIPT",
+             HasArg::Yes, Occur::Optional);
+    opts.optflag("h", "help", "display this help text and exit");
+    opts.optflag("v", "version", "display version of whirl");
+
+    let matches = match opts.parse(&argv[1..]) {
+        Ok(m) => {
+            m
+        },
+        Err(f) => {
+            eprintln!("Error: {0}", f.to_string());
+            process::exit(1);
+        }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(opts);
+        process::exit(0);
+    }
+
+    if matches.opt_present("v") {
+        println!("whirl - {0}", VERSION);
+        process::exit(0);
+    }
+
+    let script = matches.opt_str("s");
+    let _scenario = match script {
+        None => {
+            print_usage(opts);
+            process::exit(1);
+        },
+        Some(file) => {
+            Path::new(&file);
+        }
+    };
+
+    process::exit(0);
 }
